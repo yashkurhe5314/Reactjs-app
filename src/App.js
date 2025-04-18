4a5e1eb33034faa1fe490eaa6e9dfd7bef8763d4
yashkurhe5314 <yashkurhe4316@gmail.com>
2025-04-18 10:55:11 +0530

    New App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <h1>hello world</h1>
+//     </div>
+//     }
+//   );
+// }
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <button onClick={() => alert('Hello, world!')}>I'm a button</button>
+    // <button onClick={() => alert('Hello, world!')}>I'm a button</button>
+    // <div>
+    //   {alert('Hello, world!')}
+    // </div>
+    <>
+  {/* Hello world */}
+  <div className="awesome" style={{ border: "1px solid red" }}>
+    <label htmlFor="name">Enter your name: </label>
+    <input type="text" id="name" />
+  </div>
+  <p>Enter your HTML here</p>
+  <div className="awesome" style={{ border: "1px solid red" }}>
+    <label htmlFor="name">Enter your name: </label>
+    <input type="text" id="name" />
+  </div>
+  <p>Enter your HTML here</p>
+</>
+
   );
 }
+export default MyButton;
 
-export default App;
